f7c6b58bd3afca223348d410d7d6400b47abf93f
shawnc8160 <shawnc8160@gmail.com>
2018-02-21 22:53:10 -0800

    Implemented locale and able to switch the main screen from login screen.

--- src/shawn_cheng/MainApp.java
+++ src/shawn_cheng/MainApp.java
@@ -3,11 +3,11 @@ package shawn_cheng;
 import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-import shawn_cheng.view_controller.*;
-import static javafx.application.Application.launch;
+import shawn_cheng.controller.*;
 
 /**
  * This is the main class for the inventory app. 
@@ -25,46 +25,58 @@ public class MainApp extends Application {
     
     /**
      * Constructor
-     * Create inventory and add sample data to it
      */
-    public MainApp() {
-
-        
-    }
+    public MainApp() { }
     
     /**
      * Start method to set title and create initial layout
      * @param primaryStage 
      */
-
     @Override
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
         this.primaryStage.setTitle("Maddy's Scheduling App");
-        initMainLayout();
-
+        displayLogin();
     }
 
     /**
-     * Load main screen 
+     * Load login screen
      */
-    public void initMainLayout() {
+    public void displayLogin() {
         try {
-            // Load Main layout from fxml file.
             FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApp.class.getResource("view_controller/LoginScreen.fxml"));
-            mainLayout = (AnchorPane) loader.load();
-            LoginScreenController controller = loader.getController();
-            // Show the scene containing the main layout.
-            Scene scene = new Scene(mainLayout);
+            loader.setLocation(MainApp.class.getResource("views/LoginScreen.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
             primaryStage.setScene(scene);
+            LoginScreenController controller = loader.getController();
+            controller.setMainApp(this);
             primaryStage.show();
-            System.out.println("Displaying main screen");
+            System.out.println("Displaying Login screen");
         } catch (IOException e) {
             e.printStackTrace();
-
         }
     }
+
+    /**
+     * Load Main Menu screen
+     */
+    public void displayMain() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("views/MainScreen.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            // LoginScreenController controller = loader.getController();
+            // Might want to give a reference back to this object from controller.
+            primaryStage.show();
+            System.out.println("Displaying Main screen");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 
     /**
      * Main method, used to launch the application
